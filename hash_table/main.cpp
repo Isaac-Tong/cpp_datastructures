b3612eeb0c4ffd6cbe99ee0a0479fb860b2b3aac
Isaac Tong <isaact943@outlook.com>
2020-09-04 19:30:14 -0500

    Hash Maps

---
+++ hash_table/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main(){
+    unordered_map<string, double> constants;
+    
+    string pi = "pie";
+    string e = "e";
+
+    constants[pi] = 3.1415;
+    constants[e] = 2.14;
+
+
+
+}
